fea970ee53effbf9862c31c000740170f94d9fd1
martinteoharov <martin.teoharov15@gmail.com>
2021-01-28 10:29:19 +0000

    parser bug fixes

--- modules/parser.js
+++ modules/parser.js
@@ -5,28 +5,50 @@ const parse = (input) => {
 	const projects = [];
 	const tags = [];
 
+    const last = { type: null, input: null };
+
 	for( let i = 0; i < input.length; i ++ ){
+
 		if(input[i] === tokens.project){
 			let end = input.indexOf(" ", i);
-			end === -1 ? end = input.length : null;
 
-			if(end !== -1){
-				const str = input.slice(i + 1, end);
-				projects.push(str);
+            // There has been a token, but the string isn't yet finished
+            if(end === -1){
+                end = input.length;
+                const notFinishedStr = input.slice(i + 1, end);
+
+                last.type = "project";
+                last.input = notFinishedStr;
+
+            }
+
+            // There has been a token and the string is finished
+			else {
+				const finishedStr = input.slice(i + 1, end);
+				projects.push(finishedStr);
 			}
 		}
 
 		if(input[i] === tokens.tag){
 			let end = input.indexOf(" ", i);
-			end === -1 ? end = input.length : null;
 
-			if(end !== -1){
-				const str = input.slice(i + 1, end);
-				tags.push(str);
+            // There has been a token, but the string isn't yet finished
+            if(end === -1){
+                end = input.length;
+                const notFinishedStr = input.slice(i + 1, end);
+
+                last.type = "tag";
+                last.input = notFinishedStr;
+            }
+
+            // There has been a token and the string is finished
+            else {
+				const finishedStr = input.slice(i + 1, end);
+				tags.push(finishedStr);
 			}
 		}
 	}
-	return { tokens: { projects, tags } };
+	return { tokens: { projects, tags }, last };
 }
 
 // Used to parse innerHTMLs (which evaluate ' ' as 'nbsp;' )
